4b1f101a806e5519928bcf1205720bd9cfa244d1
Alan Christie <alan.christie@matildapeak.com>
2018-03-22 17:26:42 +0000

    - Initial files (liften from pipelines-utils)

---
+++ src/python/README.rst
@@ -0,0 +1,15 @@
+Informatics Matters Pipelines Utilities (RDKit)
+===============================================
+
+The ``im-pipelines-utils-rdkit`` module is a core set of utilities employed by
+`Informatics Matters`_' computational pipelines and distributed in a number of
+our container images.
+
+The utility modules provide convenient wrappers around the
+corresponding ``rdkit`` distribution.
+
+``rdkit_utils`` depends on the RDKit_ open source cheminformatics software,
+which you will need to install separately if you need to use this module.
+
+.. _RDKit: http://www.rdkit.org
+.. _Informatics Matters: http://www.informaticsmatters.com
---
+++ src/python/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Setup module for the Python-based RDKit Pipelines Utilities.
+#
+# Mar 2018
+
+from setuptools import setup, find_packages
+
+def get_long_description():
+    return open('README.rst').read()
+
+setup(
+
+    name='im-pipelines-utils-rdkit',
+    version='1.0.0',
+    author='Alan Christie',
+    author_email='achristie@informaticsmatters.com',
+    url='https://github.com/InformaticsMatters/pipelines-utils-rdkit',
+    license='Copyright (C) 2018 Informatics Matters Ltd. All rights reserved.',
+    description='Utilities for Informatics Matters Pipelines',
+    long_description=get_long_description(),
+    keywords='pipelines rdkit',
+    platforms=['any'],
+
+    # Our modules to package
+    packages=find_packages(exclude=['*.test', '*.test.*', 'test.*', 'test']),
+
+    # Essential dependencies
+    install_requires=[
+        'future >= 0.16.0'
+    ],
+    # Supported Python versions
+    # 2.7 and 3.5 or better
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, != 3.4.*, <4',
+
+    # Project classification:
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Build Tools',
+    ],
+
+    # Root of the test suite
+    test_suite = 'test',
+
+    zip_safe=False,
+
+)
